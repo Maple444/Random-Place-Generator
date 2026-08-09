160754f7b4dd54823dccaad52e52e70ba3faebea
Maple444 <slip.space@tutanota.com>
2026-08-09 14:10:35 -0300

    Previously written code

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello. Let's go somewhere.");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Choose your option:");
+        System.out.println("1. Random Country");
+        System.out.println("1. Random Continent");
+        System.out.println("1. Random City");
+    }
+}
